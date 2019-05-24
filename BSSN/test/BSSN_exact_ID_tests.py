187f6e46a3a3f0ed0f14887a0896b7d678850d5f
Kevin Lituchy <kml266@cornell.edu>
2019-05-24 13:50:23 -0400

    -Fixed numerical error -Updated trusted values -Added logging for module start / completion

--- BSSN/test/BSSN_exact_ID_tests.py
+++ BSSN/test/BSSN_exact_ID_tests.py
@@ -19,8 +19,11 @@ import BSSN.UIUCBlackHole as ubh
 
 import BSSN.ADM_Exact_Spherical_or_Cartesian_to_BSSNCurvilinear as AtoB
 
-import hashlib
 import sys
+import logging
+
+mp.dps = tv.precision
+
 
 # Lists of modules
 cartMods = [bl]
@@ -36,7 +39,6 @@ sphSumADM = [tv.BSSN_sph_SKS_ADM,tv.BSSN_sph_ST_ADM,tv.BSSN_sph_UBH_ADM]
 cartSumID = [tv.BSSN_cart_BL_ID]
 sphSumID = [tv.BSSN_sph_SKS_ID,tv.BSSN_sph_ST_ID,tv.BSSN_sph_UBH_ID]
 
-mp.dps = tv.precision
 
 # Python unittest class
 class TestStringMethods(unittest.TestCase):
@@ -49,21 +51,31 @@ class TestStringMethods(unittest.TestCase):
     par.set_parval_from_str("reference_metric::CoordSystem","Spherical")
     rfm.reference_metric() # Create ReU, ReDD needed for rescaling B-L initial data, generating BSSN RHSs, etc.
     
+    
     # Calling functions to initialize each module
     cartMods[0].BrillLindquist(ComputeADMGlobalsOnly = True)
     sphMods[0].ShiftedKerrSchild(ComputeADMGlobalsOnly = True)
     sphMods[1].StaticTrumpet(ComputeADMGlobalsOnly = True)
     sphMods[2].UIUCBlackHole(ComputeADMGlobalsOnly = True)
     
+    logging.basicConfig(level=logging.DEBUG)
+    
     # Testing [alpha,beta,...,K] for cartesian modules
     def test_cart_ADM(self):
+        
         for mod, trusted_list in zip(cartMods,cartSumADM):
+            
+            logging.info('\nCurrently working on ADM module ' + str(mod))
+            good = True
             
             # Creates list of parameters
             lst = createADMList(mod.alphaCart,mod.betaCartU,mod.BCartU,mod.gammaCartDD,mod.KCartDD)
             
             # Creates list of values
             result_list = listToValueList(lst)
+            
+            # Uncomment following line if need to calculate trustedValue for the first time
+            # tv.firstTimePrint(mod,result_list,trusted_list)
 
             # Next we compute the log_10 of the relative error. It should
             #    be a number < -2/3 * precision (i.e., when precision is 30, we
@@ -74,26 +86,30 @@ class TestStringMethods(unittest.TestCase):
                     log10_relative_error = log10(fabs(res))
                 else:
                     log10_relative_error = log10(fabs( (val - res ) / val ) )
-                self.assertTrue(log10_relative_error < tv.precision * (-2/3))
+                good = (log10_relative_error < (tv.precision / -2)) and good
+            
+            if not good:
+                logging.error('\n\n Failed with ' + str(mod) + '\n\n')
+            else:
+                logging.info('\nJust completed ADM module ' + str(mod))
+            self.assertTrue(good)
+            
                 
     # Testing [alpha,beta,...,K] for spherical modules
     def test_sph_ADM(self):
         for mod, trusted_list in zip(sphMods,sphSumADM):
             
-            print
-            print("module = ",mod)
-            print
+            logging.info('\nCurrently working on ADM module ' + str(mod))
+            good = True
+            
             # Creates list of parameters
             lst = createADMList(mod.alphaSph,mod.betaSphU,mod.BSphU,mod.gammaSphDD,mod.KSphDD)
             
             # Creates list of values
             result_list = listToValueList(lst)
             
-            # Uncomment if need to calculate trustedValue for the first time
-            # print(mod)
-            # print(result_list)
-            print
-            print(trusted_list)
+            # Uncomment following line if need to calculate trustedValue for the first time
+            # tv.firstTimePrint(mod,result_list,trusted_list)
             
             
             # Next we compute the log_10 of the relative error. It should
@@ -105,84 +121,101 @@ class TestStringMethods(unittest.TestCase):
                     log10_relative_error = log10(fabs(res))
                 else:
                     log10_relative_error = log10(fabs( (val - res ) / val ) )
-                self.assertTrue(log10_relative_error < tv.precision * (-2/3))                
-        
-#     # Testing [cf,hDD,...,bet] for cartesian modules
-#     def test_cart_ID(self):
-#         for mod, trusted_list in zip(cartMods,cartSumID):
-#             cf,hDD,lambdaU,aDD,trK,alpha,vetU,betU = AtoB.Convert_Spherical_or_Cartesian_ADM_to_BSSN_curvilinear( "Cartesian", mod.Cartxyz, mod.gammaCartDD,mod.KCartDD, mod.alphaCart, mod.betaCartU, mod.BCartU)
+                good = (log10_relative_error < (tv.precision / -2)) and good
             
-#             lst = createIDList(cf,hDD,lambdaU,aDD,trK,alpha,vetU,betU)
+            if not good:
+                logging.error('\n\n Failed with ' + str(mod) + '\n\n')
+            else:
+                logging.info('\nJust completed ADM module ' + str(mod))
+            self.assertTrue(good)
             
-#             result_list = listToValueList(lst)
+    # Testing [cf,hDD,...,bet] for cartesian modules
+    def test_cart_ID(self):
+        for mod, trusted_list in zip(cartMods,cartSumID):
             
-#             # Uncomment if need to calculate trustedValue for the first time
-#             # print(mod)
-#             # print(result_list)
+            logging.info('\nCurrently working on ID module ' + str(mod))
+            good = True
+            
+            cf,hDD,lambdaU,aDD,trK,alpha,vetU,betU = AtoB.Convert_Spherical_or_Cartesian_ADM_to_BSSN_curvilinear( "Cartesian", mod.Cartxyz, mod.gammaCartDD,mod.KCartDD, mod.alphaCart, mod.betaCartU, mod.BCartU)
+            
+            lst = createIDList(cf,hDD,lambdaU,aDD,trK,alpha,vetU,betU)
+            
+            result_list = listToValueList(lst)
+            
+            # Uncomment following line if need to calculate trustedValue for the first time
+            # tv.firstTimePrint(mod,result_list,trusted_list)
+            
+            for res, val in zip(result_list, trusted_list):
+                if val == 0:
+                    log10_relative_error = log10(fabs(res))
+                else:
+                    log10_relative_error = log10(fabs( (val - res ) / val ) )
+                good = (log10_relative_error < (tv.precision / -2)) and good
+            
+            if not good:
+                logging.error('\n\n Failed with ' + str(mod) + '\n\n')
+            else:
+                logging.info('\nJust completed ID module ' + str(mod))
+            self.assertTrue(good)
+            
+    # Testing [cf,hDD,...,bet] for spherical modules
+    def test_sph_ID(self):
+        for mod, trusted_list in zip(sphMods,sphSumID):
+                        
+            logging.info('\nCurrently working on ID module ' + str(mod))
+            good = True
+            
+            Sph_r_th_ph = [mod.r,mod.th,mod.ph]
+            cf,hDD,lambdaU,aDD,trK,alpha,vetU,betU = AtoB.Convert_Spherical_or_Cartesian_ADM_to_BSSN_curvilinear( "Spherical", Sph_r_th_ph, mod.gammaSphDD, mod.KSphDD, mod.alphaSph, mod.betaSphU, mod.BSphU)
+            
+            lst = createIDList(cf,hDD,lambdaU,aDD,trK,alpha,vetU,betU)
 
-#             for res, val in zip(result_list, trusted_list):
-#                 if val == 0:
-#                     log10_relative_error = log10(fabs(res))
-#                 else:
-#                     log10_relative_error = log10(fabs( (val - res ) / val ) )
-#                 self.assertTrue(log10_relative_error < tv.precision * (-2/3))   
+            result_list = listToValueList(lst)
             
-            
-#     # Testing [cf,hDD,...,bet] for spherical modules
-#     def test_sph_ID(self):
-#         for mod, trusted_list in zip(sphMods,sphSumID):
-#             Sph_r_th_ph = [mod.r,mod.th,mod.ph]
-#             cf,hDD,lambdaU,aDD,trK,alpha,vetU,betU = AtoB.Convert_Spherical_or_Cartesian_ADM_to_BSSN_curvilinear( "Spherical", Sph_r_th_ph, mod.gammaSphDD, mod.KSphDD, mod.alphaSph, mod.betaSphU, mod.BSphU)
-            
-#             lst = createIDList(cf,hDD,lambdaU,aDD,trK,alpha,vetU,betU)
+            # Uncomment following line if need to calculate trustedValue for the first time
+            # tv.firstTimePrint(mod,result_list,trusted_list)
 
-#             result_list = listToValueList(lst)
+            for res, val in zip(result_list, trusted_list):
+                if val == 0:
+                    log10_relative_error = log10(fabs(res))
+                else:
+                    log10_relative_error = log10(fabs( (val - res ) / val ) )
+                good = (log10_relative_error < (tv.precision / -2)) and good
             
-#             # Uncomment if need to calculate trustedValue for the first time
-#             print(mod)
-#             print(result_list)
+            if not good:
+                logging.error('\n\n Failed with ' + str(mod) + '\n\n')
+            else:
+                logging.info('\nJust completed ID module ' + str(mod))
+            self.assertTrue(good)
+            
 
-#             for res, val in zip(result_list, trusted_list):
-#                 if val == 0:
-#                     log10_relative_error = log10(fabs(res))
-#                 else:
-#                     log10_relative_error = log10(fabs( (val - res ) / val ) )
-#                 self.assertTrue(log10_relative_error < (tv.precision / -2))
-
+                
 ################################
 ######## Subfunctions ##########
 ################################
-            
-# def createIDList(cf,hDD,lambdaU,aDD,trK,alpha,vetU,betU):
-#     lst = [cf,alpha,trK]
-#     for i in range(3):
-#         lst.append(lambdaU[i])
-#         lst.append(vetU[i])
-#         lst.append(betU[i])
-#         for j in range(i,3):
-#             lst.append(hDD[i][j])
-#             lst.append(aDD[i][j])
-#     return lst 
+
+# Creates [lst] which contains all information stored in inputs [cf,...,betU]
+def createIDList(cf,hDD,lambdaU,aDD,trK,alpha,vetU,betU):
+    lst = [cf,alpha,trK]
+    for i in range(3):
+        lst.append(lambdaU[i])
+        lst.append(vetU[i])
+        lst.append(betU[i])
+        for j in range(i,3):
+            lst.append(hDD[i][j])
+            lst.append(aDD[i][j])
+    return lst 
             
 # Creates [lst] which contains all information stored in inputs [alpha,...,K]
 def createADMList(alpha,beta,B,gamma,K):
     
-    slst = ['alpha']
     lst = [alpha]
     for i in range(3):
         lst.append(beta[i])
         lst.append(B[i])
-        slst.append('beta ' + str(i))
-        slst.append('B ' + str(i))
         for j in range(3):
             lst.append(gamma[i][j])
             lst.append(K[i][j])   
-            slst.append('gamma (' + str(i) + ',' + str(j) + ')')
-            slst.append('K (' + str(i) + ',' + str(j) + ')')
-    print
-    print(lst)
-    print
-    print(slst)
     return lst       
 
 # Takes in a list [lst] and returns the list with each index evaluated 
