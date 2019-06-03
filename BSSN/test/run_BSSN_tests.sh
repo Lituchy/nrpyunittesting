fea1f4e46170d4b459b82d1912d4ea20e1e1a5f2
Zachariah Etienne <zachetie@gmail.com>
2019-06-02 20:28:23 -0400

    Update Psi4 tetrad module & trusted values so that the unit testing works, and we maintain high precision in 1/sqrt(2) when needed.

--- BSSN/test/run_BSSN_tests.sh
+++ BSSN/test/run_BSSN_tests.sh
@@ -2,8 +2,8 @@
 
 export PYTHONPATH=$PYTHONPATH:`pwd`
 
-pypy BSSN/test/BSSN_exact_ID_tests.py &&
-pypy BSSN/test/BSSN_ADM_Test.py #&&
-# pypy BSSN/test/BSSN_Psi4_test.py #&&
+#pypy BSSN/test/BSSN_exact_ID_tests.py &&
+#pypy BSSN/test/BSSN_ADM_Test.py &&
+pypy BSSN/test/BSSN_Psi4_test.py #&&
 # pypy BSSN/test/BSSN_RHS_test.py
 
