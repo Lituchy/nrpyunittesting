a33bc54f28a6737fa4f585140fe5340145de740d
Kevin Lituchy <kml266@cornell.edu>
2019-06-18 12:25:14 -0400

    -Fixed precision error: now all trusted values have a precision of 30

--- BSSN/test/NRPyUnitTests_Globals_Tests.py
+++ BSSN/test/NRPyUnitTests_Globals_Tests.py
@@ -13,13 +13,9 @@ from RepeatedTimer import RepeatedTimer
 logging.basicConfig(level=logging.INFO)
 
 
-# # https://stackoverflow.com/questions/3393612/run-certain-code-every-n-seconds/13151299
-# # Creates a threaded timer object that prints to the console every 5 minutes
-def print_fun(msg):
-    logging.info(msg)
-
-
-Timer = RepeatedTimer(300, print_fun, "\nPrinting every 5 minutes to prevent timeouts.\n")
+# https://stackoverflow.com/questions/3393612/run-certain-code-every-n-seconds/13151299
+# Creates a threaded timer object that prints to the console every 5 minutes
+Timer = RepeatedTimer(300, logging.info, "\nPrinting every 5 minutes to prevent timeouts.\n")
 
 
 # Python unittest class
