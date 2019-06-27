7ac16eb69dbdf064146d4eff945e4bfd3ede773c
Kevin Lituchy <kml266@cornell.edu>
2019-06-27 16:51:46 -0400

    -Enabling all tests for end-of-day push

--- UnitTesting/run_NRPy_UnitTests.sh
+++ UnitTesting/run_NRPy_UnitTests.sh
@@ -14,13 +14,13 @@ echo $PYTHONEXEC version info:
 $PYTHONEXEC --version
 echo "########################################"
 
-$PYTHONEXEC UnitTesting/NRPyUnitTests_Functions.py #&&
-#$PYTHONEXEC BSSN/tests/NRPyUnitTests_BSSN_Globals.py &&
-#$PYTHONEXEC tests/NRPyUnitTests_Reference_Metric_Globals.py &&
-#$PYTHONEXEC FishboneMoncriefID/tests/NRPyUnitTests_FishboneMoncriefID_Globals.py &&
-#$PYTHONEXEC GiRaFFE_HO/tests/NRPyUnitTests_GiRaFFE_HO_Globals.py &&
-#$PYTHONEXEC GiRaFFEfood_HO/tests/NRPyUnitTests_GiRaFFEfood_HO_Globals.py &&
-#$PYTHONEXEC ScalarWave/tests/NRPyUnitTests_ScalarWave_Globals.py &&
-#$PYTHONEXEC ScalarWaveCurvilinear/tests/NRPyUnitTests_ScalarWaveCurvilinear_Globals.py &&
-#$PYTHONEXEC Maxwell/tests/NRPyUnitTests_Maxwell_Globals.py
-#$PYTHONEXEC u0_smallb_Poynting__Cartesian/tests/NRPyUnitTests_u0_smallb_Poynting__Carteisan_Globals.py
+$PYTHONEXEC UnitTesting/NRPyUnitTests_Functions.py &&
+$PYTHONEXEC BSSN/tests/NRPyUnitTests_BSSN_Globals.py &&
+$PYTHONEXEC tests/NRPyUnitTests_Reference_Metric_Globals.py &&
+$PYTHONEXEC FishboneMoncriefID/tests/NRPyUnitTests_FishboneMoncriefID_Globals.py &&
+$PYTHONEXEC GiRaFFE_HO/tests/NRPyUnitTests_GiRaFFE_HO_Globals.py &&
+$PYTHONEXEC GiRaFFEfood_HO/tests/NRPyUnitTests_GiRaFFEfood_HO_Globals.py &&
+$PYTHONEXEC ScalarWave/tests/NRPyUnitTests_ScalarWave_Globals.py &&
+$PYTHONEXEC ScalarWaveCurvilinear/tests/NRPyUnitTests_ScalarWaveCurvilinear_Globals.py &&
+$PYTHONEXEC Maxwell/tests/NRPyUnitTests_Maxwell_Globals.py
+$PYTHONEXEC u0_smallb_Poynting__Cartesian/tests/NRPyUnitTests_u0_smallb_Poynting__Carteisan_Globals.py
