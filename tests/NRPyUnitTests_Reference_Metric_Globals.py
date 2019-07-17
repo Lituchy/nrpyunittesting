57fed3dc8ea8b0e9e150bea50369e8bbfa7b2dd9
Kevin Lituchy <kml266@cornell.edu>
2019-07-17 12:47:50 -0400

    -Added reload to every unittest of reference_metric for testing purposes

--- tests/NRPyUnitTests_Reference_Metric_Globals.py
+++ tests/NRPyUnitTests_Reference_Metric_Globals.py
@@ -5,6 +5,7 @@ from UnitTesting.run_test import run_test
 from UnitTesting.functions_and_globals import functions_and_globals
 from UnitTesting.RepeatedTimer import RepeatedTimer
 from UnitTesting.setup_trusted_values_dict import setup_class
+import sys
 
 # TODO: Change level based on desired amount of output.
 # ERROR -> Outputs minimal information -- only when there's an error
@@ -25,7 +26,6 @@ class TestGlobals(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         # Creating trusted_values_dict.py if it doesn't exist
-        import sys
         global path
         path = sys.path[0]
         setup_class(path)
@@ -33,22 +33,6 @@ class TestGlobals(unittest.TestCase):
     @classmethod
     def tearDownClass(cls):
         Timer.stop()
-
-    def tearDown(self):
-        import sys
-        import reference_metric
-        #import NRPy_param_funcs as par
-        if sys.version_info[0] == 2:
-            reload(reference_metric)
-            print('v2')
-        elif sys.version_info[1] <= 3:
-            import imp
-            imp.reload(reference_metric)
-            print('v3.3')
-        else:
-            import importlib
-            importlib.reload(reference_metric)
-            print('v3.4+')
 
     # Testing globals for reference_metric
     def ftest_reference_metric_globals(self):
@@ -113,6 +97,15 @@ class TestGlobals(unittest.TestCase):
 
         run_test(self, mod_dict, trusted_values_dict.trusted_values_dict, path, locals())
 
+        if sys.version_info[0] == 2:
+            reload(rfm_Spherical)
+        elif sys.version_info[1] <= 3:
+            import imp
+            imp.reload(rfm_Spherical)
+        else:
+            import importlib
+            importlib.reload(rfm_Spherical)
+
     def test_SinhSpherical(self):
 
         import trusted_values_dict
@@ -127,6 +120,15 @@ class TestGlobals(unittest.TestCase):
         mod_dict = {'rfm_SinhSpherical': functions_and_globals(['reference_metric(True)'], global_list)}
 
         run_test(self, mod_dict, trusted_values_dict.trusted_values_dict, path, locals())
+
+        if sys.version_info[0] == 2:
+            reload(rfm_SinhSpherical)
+        elif sys.version_info[1] <= 3:
+            import imp
+            imp.reload(rfm_SinhSpherical)
+        else:
+            import importlib
+            importlib.reload(rfm_SinhSpherical)
 
     def test_SinhSphericalv2(self):
 
@@ -143,6 +145,15 @@ class TestGlobals(unittest.TestCase):
 
         run_test(self, mod_dict, trusted_values_dict.trusted_values_dict, path, locals())
 
+        if sys.version_info[0] == 2:
+            reload(rfm_SinhSphericalv2)
+        elif sys.version_info[1] <= 3:
+            import imp
+            imp.reload(rfm_SinhSphericalv2)
+        else:
+            import importlib
+            importlib.reload(rfm_SinhSphericalv2)
+
     def test_NobleSphericalThetaOptionOne(self):
 
         import trusted_values_dict
@@ -157,6 +168,15 @@ class TestGlobals(unittest.TestCase):
         mod_dict = {'rfm_NobleSphericalThetaOptionOne': functions_and_globals(['reference_metric(False)'], global_list)}
 
         run_test(self, mod_dict, trusted_values_dict.trusted_values_dict, path, locals())
+
+        if sys.version_info[0] == 2:
+            reload(rfm_NobleSphericalThetaOptionOne)
+        elif sys.version_info[1] <= 3:
+            import imp
+            imp.reload(rfm_NobleSphericalThetaOptionOne)
+        else:
+            import importlib
+            importlib.reload(rfm_NobleSphericalThetaOptionOne)
 
     def test_NobleSphericalThetaOptionTwo(self):
 
@@ -173,6 +193,15 @@ class TestGlobals(unittest.TestCase):
 
         run_test(self, mod_dict, trusted_values_dict.trusted_values_dict, path, locals())
 
+        if sys.version_info[0] == 2:
+            reload(rfm_NobleSphericalThetaOptionTwo)
+        elif sys.version_info[1] <= 3:
+            import imp
+            imp.reload(rfm_NobleSphericalThetaOptionTwo)
+        else:
+            import importlib
+            importlib.reload(rfm_NobleSphericalThetaOptionTwo)
+
     def test_Cylindrical(self):
 
         import trusted_values_dict
@@ -187,6 +216,15 @@ class TestGlobals(unittest.TestCase):
         mod_dict = {'rfm_Cylindrical': functions_and_globals(['reference_metric(True)'], global_list)}
 
         run_test(self, mod_dict, trusted_values_dict.trusted_values_dict, path, locals())
+
+        if sys.version_info[0] == 2:
+            reload(rfm_Cylindrical)
+        elif sys.version_info[1] <= 3:
+            import imp
+            imp.reload(rfm_Cylindrical)
+        else:
+            import importlib
+            importlib.reload(rfm_Cylindrical)
 
     def test_SinhCylindrical(self):
 
@@ -203,6 +241,15 @@ class TestGlobals(unittest.TestCase):
 
         run_test(self, mod_dict, trusted_values_dict.trusted_values_dict, path, locals())
 
+        if sys.version_info[0] == 2:
+            reload(rfm_SinhCylindrical)
+        elif sys.version_info[1] <= 3:
+            import imp
+            imp.reload(rfm_SinhCylindrical)
+        else:
+            import importlib
+            importlib.reload(rfm_SinhCylindrical)
+
     def test_SinhCylindricalv2(self):
 
         import trusted_values_dict
@@ -217,6 +264,15 @@ class TestGlobals(unittest.TestCase):
         mod_dict = {'rfm_SinhCylindricalv2': functions_and_globals(['reference_metric(True)'], global_list)}
 
         run_test(self, mod_dict, trusted_values_dict.trusted_values_dict, path, locals())
+
+        if sys.version_info[0] == 2:
+            reload(rfm_SinhCylindricalv2)
+        elif sys.version_info[1] <= 3:
+            import imp
+            imp.reload(rfm_SinhCylindricalv2)
+        else:
+            import importlib
+            importlib.reload(rfm_SinhCylindricalv2)
 
     def test_SymTP(self):
 
@@ -233,6 +289,15 @@ class TestGlobals(unittest.TestCase):
 
         run_test(self, mod_dict, trusted_values_dict.trusted_values_dict, path, locals())
 
+        if sys.version_info[0] == 2:
+            reload(rfm_SymTP)
+        elif sys.version_info[1] <= 3:
+            import imp
+            imp.reload(rfm_SymTP)
+        else:
+            import importlib
+            importlib.reload(rfm_SymTP)
+
     def test_SinhSymTP(self):
 
         import trusted_values_dict
@@ -248,6 +313,15 @@ class TestGlobals(unittest.TestCase):
 
         run_test(self, mod_dict, trusted_values_dict.trusted_values_dict, path, locals())
 
+        if sys.version_info[0] == 2:
+            reload(rfm_SinhSymTP)
+        elif sys.version_info[1] <= 3:
+            import imp
+            imp.reload(rfm_SinhSymTP)
+        else:
+            import importlib
+            importlib.reload(rfm_SinhSymTP)
+
     def test_Cartesian(self):
 
         import trusted_values_dict
@@ -262,6 +336,15 @@ class TestGlobals(unittest.TestCase):
         mod_dict = {'rfm_Cartesian': functions_and_globals(['reference_metric(True)'], global_list)}
 
         run_test(self, mod_dict, trusted_values_dict.trusted_values_dict, path, locals())
+
+        if sys.version_info[0] == 2:
+            reload(rfm_Cartesian)
+        elif sys.version_info[1] <= 3:
+            import imp
+            imp.reload(rfm_Cartesian)
+        else:
+            import importlib
+            importlib.reload(rfm_Cartesian)
 
 
 # Necessary for unittest class to work properly
